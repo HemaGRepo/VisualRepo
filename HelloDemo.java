87224004835ec14d3d901fbc3cb952f3354935b1
HemaGRepo <hema.g@capgemini.com>
2018-06-15 12:41:50 +0530

    Add files via upload

---
+++ HelloDemo.java
@@ -0,0 +1,17 @@
+
+public class HelloDemo {
+
+	public static void main(String[] args) {
+		System.out.println("*********************");
+		System.out.println("Welcome to Jenkins - GitHub");
+		System.out.println("*********************");
+		int cntr=0;
+		for(cntr=0;cntr<5;cntr++)
+		{
+			System.out.println("Executing ..."+cntr);
+		}
+		System.out.println("******* Thanks for using GitHub **************");
+
+	}
+
+}
